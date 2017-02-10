39e2e3c9361b3fc841cef016f8f54d7d22198ad2
NickWydra <njwydra@umich.edu>
2017-02-10 11:26:56 -0500

    reacts to time

--- static/js/animate.js
+++ static/js/animate.js
@@ -6,26 +6,28 @@
     window.msRequestAnimationFrame;
     window.requestAnimationFrame = requestAnimationFrame;
 })();
- 
 
+  var time = new Date();
+  var minute = time.getMinutes();
+  var hour = time.getHours();
+ 
   var canvas = document.getElementById('myCanvas');
   var context = canvas.getContext('2d');
   var x = canvas.width / 2;
   var y = canvas.height / 2;
-  var radius = 75;
-  var endPercent = 85;
+  var radius = 100;
+  var endPercent = minute / 60 * 100;
   var curPerc = 0;
   var counterClockwise = false;
   var circ = Math.PI * 2;
   var quart = Math.PI / 2;
 
-  context.lineWidth = 10;
+  context.lineWidth = 8;
   context.strokeStyle = '#ad2323';
   context.shadowOffsetX = 0;
   context.shadowOffsetY = 0;
-  context.shadowBlur = 10;
+  context.shadowBlur = 0;
   context.shadowColor = '#656565';
-
 
   function animate(current) {
     context.clearRect(0, 0, canvas.width, canvas.height);
@@ -41,3 +43,10 @@
   }
 
  animate();
+
+ var canvas2 = document.getElementById('myCanvas2');
+ var context2 = canvas2.getContext('2d');
+ context2.font = "100px Arial";
+ context2.textAlign = "center";
+ context2.textBaseline = "middle"
+ context2.fillText(hour,x,125);
